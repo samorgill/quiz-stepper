04705b0811c093e19828e2446def2476d468b973
Sam Orgill <ssorgill@icloud.com>
2020-06-14 16:45:42 +0100

    Questions

---
+++ src/app/_helpers/QUESTIONS.ts
@@ -0,0 +1,60 @@
+import {QuestionModel} from './question.model';
+
+export const QUESTIONS: QuestionModel[] = [
+  {
+    question: 'McDonald’s once made bubblegum-flavored broccoli',
+    answer: true
+  },
+  {
+    question: 'The first oranges weren’t orange',
+    answer: true
+  },
+  {
+    question: 'The statue of Liberty was gifted to the USA by Britain',
+    answer: false
+  },
+  {
+    question: 'The statue of Liberty was gifted to the USA by Britain',
+    answer: false
+  },
+  {
+    question: 'A cow-bison hybrid is called a “beefalo”',
+    answer: true
+  },
+  {
+    question: 'The capital of Australia is Sydney',
+    answer: false
+  },
+  {
+    question: 'Armadillo shells are bulletproof',
+    answer: true
+  },
+  {
+    question: 'HTML stands for How To Meet Ladies',
+    answer: false
+  },
+  {
+    question: 'The Sahara is the largest desert in the world',
+    answer: false
+  },
+  {
+    question: 'There were active volcanoes on the moon when dinosaurs were alive',
+    answer: true
+  },
+  {
+    question: 'Theodore Roosevelt had a pet hyena',
+    answer: true
+  },
+  {
+    question: 'Theodore Roosevelt had a pet hyena',
+    answer: true
+  },
+  {
+    question: 'Giraffes tongues are pink',
+    answer: false
+  },
+  {
+    question: 'Giraffes tongues are pink',
+    answer: false
+  }
+];
